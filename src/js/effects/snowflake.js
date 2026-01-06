8b74ece85998889be3cd267256ca23b8d22ef4b4
mjsoftware <1406993111@qq.com>
2026-01-06 10:35:42 +0800

    修复安全连接雪花飘落特效不显示问题

--- src/js/effects/snowflake.js
+++ src/js/effects/snowflake.js
@@ -142,5 +142,5 @@ function drawSnow() {
 }
 
 /* 调用及控制方法 */
-var snow = new snowFall({maxFlake: Utils.isMobile() ? 35 : 100})
+var snow = new snowFall({maxFlake: Utils.isMobile() ? 20 : 90})
 snow.start()
