b7d03bf60ba0e32f799c5030d747440e04d4f15c
mjsoftware <1406993111@qq.com>
2026-07-14 18:02:59 +0800

    fix(pjax): 修复页面布局和导航高亮问题

--- src/js/pjax.js
+++ src/js/pjax.js
@@ -180,6 +180,12 @@ document.addEventListener('pjax:success', async function (event) {
   window.postPjax && window.postPjax()
   /* 刷新人生倒计时 */
   commonContext.initTimeCount()
+  /* 导航高亮 */
+  document
+    .querySelectorAll('.navbar-nav .current, .panel-side-menu .current')
+    .forEach(el => el.classList.remove('current'))
+  commonContext.initNavbar()
+  /* 处理banner显示 */
   /* 初始化任务列表，禁止点击 */
   commonContext.iniTaskItemDisabled()
   /* 初始化轮播 */
